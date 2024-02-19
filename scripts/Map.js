c9afbb4a63d236fc1c9901899a49f8411cefaf53
ThomasNagel <tmj.nagel@gmail.com>
2024-02-19 10:52:16 +0100

    feat:"map" show areas that contain data

--- scripts/Map.js
+++ scripts/Map.js
@@ -1,3 +1,5 @@
+import { db } from "./Database.js";
+
 export class OLMap {
     map = null;
 
@@ -16,53 +18,58 @@ export class OLMap {
             }),
             controls: [],
         });
-    }
 
-    displayAreas(areas) {
-        const source = areaVector.getSource();
-        const total = Object.values(areas).reduce((a, b) => a + b, 0);
-        console.log(total);
-
-        source.getFeatures().forEach(feat => {
-            const name = feat.getProperties()["name"];
-
-            if (name in areas) {
-                feat.setStyle(
-                    new ol.style.Style({
-                        fill: new ol.style.Fill({
-                            color: [0, 255, 0, 1]
-                        })
-                }));
+        fetch('/data/config.json').then(res => { //Check fetch response
+            if (!res.ok) {
+                throw new Error("HTTP error " + res.status);
             }
-        });
-    }
+            
+            return res.json();
+        }).then(res => {
+            const area = res["search"]["area"]["fields"][0];
 
-    addAreas(areas) {
-        const source = areaVector.getSource();
+            db.querySQL(`SELECT "${area}", COUNT("${area}") FROM metadata GROUP BY "${area}"`).then(result => {
+                let amounts = {};
+                let total = 0;
 
-        source.on("change", ev => {
-            if (source.getState() === "ready" ) {
-                this.displayAreas(areas);
-            } else {
-                throw error;
-            }
-        });
+                for (const key in result) {
+                    amounts[result[key][area]] = result[key][`COUNT("${area}")`];
+                    total = total + result[key][`COUNT("${area}")`];
+                }
 
-        this.map.addLayer(areaVector);
+                if (!total) {
+                    total = 1;
+                }
+
+                amounts["total"] = total;
+
+                return amounts
+            }).then(amounts => {
+                return new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                        url: '/data/custom.geojson',
+                        format: new ol.format.GeoJSON()
+                    }),
+                    style: function(feature, _) {
+                        const name = feature.getProperties()["name"];
+                        
+                        let num = 0;
+                        
+                        if (name in amounts) {
+                            num = amounts[name];
+                        }
+                        
+                        const oppacity = (num / amounts["total"]) * 0.5;
+
+                        return new ol.style.Style({
+                            fill: new ol.style.Fill({
+                                color: [255, 0, 0, oppacity]
+                            })
+                        });
+                    }
+                });
+            }).then(vec => {
+                this.map.addLayer(vec);
+        })});
     }
 }
-
-const areaVector = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '/data/custom.geojson',
-        format: new ol.format.GeoJSON()
-    }),
-    style: null,
-});
-
-
-/*areaVector.getSource().on("change", function(ev) {
-    if( areaVector.getSource().getState() === "ready" ) {
-        console.log(areaVector.getSource().getFeatures())
-    }
-});*/
